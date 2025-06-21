7fb83b664670e7524997d0d45e0a673a5dc45162
Dev-Niyas <niyas.k@urolime.com>
2025-06-21 19:08:14 +0530

    Api Created for demo

---
+++ RafLab.Api/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RafLab.Application.Services;
+using RafLab.Application.Services._i;
+
+namespace RafLab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IExternalUserService _userService;
+        public UserController(IExternalUserService userservice)
+        {
+            _userService = userservice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+
+            return Ok(user);
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var user = await _userService.GetAllUsersAsync();
+
+            return Ok(user);
+        }
+
+        [HttpGet("GetCaheUsers")]
+        public async Task<IActionResult> GetCaheUsers()
+        {
+            var user = await _userService.GetCacheUser();
+
+            return Ok(user);
+        }
+    }
+}
---
+++ RafLab.Api/Program.cs
@@ -0,0 +1,55 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RafLab.Application.Services;
+using RafLab.Application.Services._i;
+using RafLab.Core.Infrastucture;
+using RafLab.Shared.Dto;
+
+namespace RafLab.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
+            builder.Services.AddHttpClient<IExternalUserService, ExternalUserService>((sp, client) =>
+            {
+                var settings = sp.GetRequiredService<IOptions<ApiSettings>>().Value;
+                client.BaseAddress = new Uri(settings.BaseUrl);
+                client.DefaultRequestHeaders.Add("x-api-key", settings.ApiKey);
+            });
+
+            builder.Services.AddDbContext<ReqresUserDbContext>(options =>
+    options.UseInMemoryDatabase("UserCacheDatabase"));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
