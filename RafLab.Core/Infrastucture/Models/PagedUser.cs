615c1167cefda0e5b4ece8bdc08a48d5b32d89c6
Dev-Niyas <niyas.k@urolime.com>
2025-06-21 19:06:26 +0530

    Core - inMemory Database configured

---
+++ RafLab.Core/Infrastucture/Models/PagedUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RafLab.Core.Infrastucture.Models
+{
+    public class PagedUser
+    {
+        public int Page { get; set; }
+        public int Per_Page { get; set; }
+        public int Total { get; set; }
+        public int Total_Pages { get; set; }
+        public List<User> Data { get; set; } = new();
+        public SupportInfo Support { get; set; } = new();
+    }
+}
---
+++ RafLab.Core/Infrastucture/ReqresUserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RafLab.Core.Infrastucture.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RafLab.Core.Infrastucture
+{
+    public class ReqresUserDbContext :DbContext
+    {
+        public ReqresUserDbContext(DbContextOptions<ReqresUserDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; } = null!;
+
+    }
+}
